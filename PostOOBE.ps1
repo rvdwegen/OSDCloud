6dba9ba25f8d7a7e055a8aa864006b03ff1f215c
Roel van der Wegen <github@aeternus.tech>
2024-02-12 20:00:35 +0100

    .

--- PostOOBE.ps1
+++ PostOOBE.ps1
@@ -9,8 +9,10 @@ try {
            Write-Host "Executionpolicy has been changed to unrestricted for this process" -ForegroundColor Green
     }
 
-    Invoke-RestMethod -Uri "https://github.com/mgajda83/PSWindowsUpdate/archive/refs/heads/main.zip" -OutFile "$env:TEMP\lol098.zip"
-    Expand-Archive -Path "$env:TEMP\lol098.zip" -DestinationPath "$env:TEMP\lol098" -Force
+    [uri]$url = "https://github.com/mgajda83/PSWindowsUpdate/archive/refs/heads/main.zip"
+    Invoke-RestMethod -Uri "https://github.com/mgajda83/PSWindowsUpdate/archive/refs/heads/main.zip" -OutFile "$env:TEMP\$($url.Segments[2].Trim('/')).zip"
+    Expand-Archive -Path "$env:TEMP\$($url.Segments[2].Trim('/')).zip" -DestinationPath "$env:PROGRAMFILES\WindowsPowerShell\Modules\$($url.Segments[2].Trim('/'))" -Force
+    C:\Program Files\WindowsPowerShell\Modules
     "$env:TEMP\lol098\PSWindowsUpdate-main\PSWindowsUpdate\*" | Get-ChildItem -include '*.psm1','*.ps1' | Import-Module
 
     # Install PackageProvider NuGet
